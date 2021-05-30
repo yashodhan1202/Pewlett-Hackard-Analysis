e9753b89cbb6c90bb4fa00cc6f2e7dafe340d4d5
yashodhan1202 <yashodhan.marathe12@gmail.com>
2021-05-30 17:28:49 -0400

    After import of data into tables created in PgAdmin

--- schema.sql
+++ schema.sql
@@ -44,4 +44,11 @@ CREATE TABLE dept_emp (
     PRIMARY KEY (emp_no, dept_no)
 );
 
-SELECT * FROM departments;
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, from_date)
+);
